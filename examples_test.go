235fc9d9f5224fd384c383231ec6d03433542b17
Andrea Parodi <andrea@parro.it>
2021-04-09 16:14:22 +0200

    add an example for writefile

--- examples_test.go
+++ examples_test.go
@@ -2,6 +2,7 @@ package lines_test
 
 import (
 	"embed"
+	"fmt"
 	"io/fs"
 	"os"
 
@@ -13,12 +14,39 @@ var fixtureRootFS embed.FS
 var fixtureFS, _ = fs.Sub(fixtureRootFS, "fixtures")
 
 // This example show how to use a lines.Buffer
-func ExampleBuffer() {
+// to acccumulate a bunch of lines and write them
+// to console at end using `Write` method.
+func ExampleBuffer_Write() {
 	var lines lines.Buffer
 	lines.AddLine("A simple line")
 	lines.AddLine("Trailing new lines are not needed\n")
 	lines.AddLine("You can use %s arguments", "fmt.Printf")
-	lines.Write(os.Stdout)
+
+	err := lines.Write(os.Stdout)
+	if err != nil {
+		panic(err)
+	}
+	// Output: A simple line
+	// Trailing new lines are not needed
+	//
+	// You can use fmt.Printf arguments
+}
+
+// This example show how to use a lines.Buffer
+func ExampleBuffer_WriteFile() {
+	var lines lines.Buffer
+	lines.AddLine("A simple line")
+	lines.AddLine("Trailing new lines are not needed\n")
+	lines.AddLine("You can use %s arguments", "fmt.Printf")
+
+	err := lines.WriteFile("/tmp/example")
+	if err != nil {
+		panic(err)
+	}
+	// read the content of the file just written
+	content, _ := os.ReadFile("/tmp/example")
+	os.Remove("/tmp/example")
+	fmt.Print(string(content))
 	// Output: A simple line
 	// Trailing new lines are not needed
 	//
